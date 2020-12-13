034a5aa0dbe8a8af0ea0e33ad1cc6183b9598095
jonnystv <jonathan@darefoundation.com>
2020-12-13 15:31:03 +0000

    Models created

---
+++ models/animal.py
@@ -0,0 +1,11 @@
+class Animal:
+    def __init__(self, name, type, dob, age, notes, owner, owner_tel, owner_email, id = None):
+        self.name = name
+        self.type = type
+        self.dob = dob
+        self.age = age
+        self.notes = notes
+        self.owner = owner
+        self.owner_tel = owner_tel
+        self.ownner_email = owner_email
+        self.id = id

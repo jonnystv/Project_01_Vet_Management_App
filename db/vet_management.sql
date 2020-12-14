95c9301ea7b0dcde8d64433cda5ab8badc827800
jonnystv <jonathan@darefoundation.com>
2020-12-14 02:49:10 +0000

    Problems saving animals to db

--- db/vet_management.sql
+++ db/vet_management.sql
@@ -6,15 +6,15 @@ CREATE TABLE vets (
     name VARCHAR(255)
 );
 
--- CREATE TABLE animals (
---     id SERIAL PRIMARY KEY,
---     name VARCHAR(255),
---     type VARCHAR(255),
---     dob VARCHAR(255),
---     age VARCHAR(255),
---     notes TEXT,
---     owner VARCHAR(255),
---     owner_tel VARCHAR(255),
---     owner_email VARCHAR(255),
---     vet_id INT REFERENCES vets(id)
---     );
+CREATE TABLE animals (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    type VARCHAR(255),
+    dob VARCHAR(255),
+    age VARCHAR(255),
+    notes TEXT,
+    owner VARCHAR(255),
+    owner_tel VARCHAR(255),
+    owner_email VARCHAR(255),
+    vet_id INT REFERENCES vets(id)
+    );
--- repositories/animal_repository.py
+++ repositories/animal_repository.py
@@ -1,8 +1,16 @@
 from db.run_sql import run_sql
 
-from models.animal import Animal
 from models.vet import Vet
+from models.animal import Animal
 
-import repositories.animal_repository as animal_repository
 import repositories.vet_repository as vet_repository
+import repositories.animal_repository as animal_repository
 
+def save(animal):
+    sql = "INSERT INTO animals( name, type, dob, age, notes, owner, owner_tel, owner_email, vet_id ) VALUES ( %s, %s, %s, %s, %s, %s, %s, %s, %s ) RETURNING id"
+    values = [animal.name, animal.type, animal.dob, animal.age, animal.notes, animal.owner,animal.owner_tel, animal.owner_email, animal.vet.id]
+    results = run_sql(sql, values)
+    animal.id = results[0]['id']
+    return animal
+
+    

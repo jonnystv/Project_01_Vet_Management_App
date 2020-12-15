53af6bbfeb08f814ac131573ab47d0bca210adf3
jonnystv <jonathan@darefoundation.com>
2020-12-15 23:02:26 +0000

    HTML and route for home page created successfully

--- console.py
+++ console.py
@@ -14,11 +14,11 @@ vet_repository.save(vet_1)
 vet_2 = Vet("Dr Herriot")
 vet_repository.save(vet_2)
 
-# vet_3 = Vet("Dr Hamster")
-# vet_repository.save(vet_3)
+vet_3 = Vet("Dr Hamster")
+vet_repository.save(vet_3)
 
-# vet_4 = Vet("Dr Greene")
-# vet_repository.save(vet_4)
+vet_4 = Vet("Dr Greene")
+vet_repository.save(vet_4)
 
 vet_repository.select_all()
 
@@ -28,12 +28,11 @@ animal_repository.save(animal_1)
 animal_2 = Animal("Twiggy", "Stick Insect", "10-10-2019", "1", "Leg pulled off by spider. Replacement leg needed", "David Bellamy", "07922175003", "david@bellamy.com", vet_1)
 animal_repository.save(animal_2)
 
-# animal_3 = Animal("Zeus", "Silverback", "17-10-1990", "30", "Ingrowing toenail, left pinky toe", "Jo Mobutu", "07922175003", "big@thief.com", vet_3)
-# animal_repository.save(animal_3)
+animal_3 = Animal("Zeus", "Silverback", "17-10-1990", "30", "Ingrowing toenail, left pinky toe", "Jo Mobutu", "07922175003", "big@thief.com", vet_3)
+animal_repository.save(animal_3)
 
-# animal_4 = Animal("Cornelius", "Chimpanzee", "12-11-1995", "25", "Nasty bruise to forehead. Possible X-Ray", "George Taylor", "07922175003", "dirty@ape.com", vet_4)
-# animal_repository.save(animal_4)
-
+animal_4 = Animal("Cornelius", "Chimpanzee", "12-11-1995", "25", "Nasty bruise to forehead. Possible X-Ray", "George Taylor", "07922175003", "dirty@ape.com", vet_4)
+animal_repository.save(animal_4)
 
 
 # pdb.set_trace()

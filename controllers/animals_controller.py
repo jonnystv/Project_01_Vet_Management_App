b667d854fe4f7378f193333d28dfdcbf72777dca
jonnystv <jonathan@darefoundation.com>
2020-12-16 12:33:53 +0000

    route and html for individual animal page completed

--- controllers/animals_controller.py
+++ controllers/animals_controller.py
@@ -13,6 +13,11 @@ def animals():
     animals = animal_repository.select_all()
     return render_template("index.html", animals = animals)
 
+@animals_blueprint.route("/animals/<id>")
+def show(id):
+    animal = animal_repository.select(id)
+    return render_template("/animals/show.html", animal = animal)
+
 @animals_blueprint.route("/animals/")
 def animals_page():
     animals = animal_repository.select_all_animals()
@@ -22,21 +27,21 @@ def animals_page():
 def new_animal():
     return render_template("/animals/new.html")
 
-# @animals_blueprint.route("/animals/", methods=["POST"])
-# def add_animal():
-#     name = request.form["name"]
-#     type = request.form["type"]
-#     dob = request.form["dob"]
-#     age = request.form["age"]
-#     notes = request.form["notes"]
-#     owner = request.form["owner"]
-#     owner_tel = request.form["owner_tel"]
-#     owner_email = request.form["owner_email"]
-#     vet_id = request.form["vet_id"]
-#     vet = vet_repository.select(vet_id)
-#     new_animal = Animal(name, type, dob, age, notes, owner, owner_email, owner_tel, vet)
-#     animal_repository.save(new_animal)
-#     return redirect("/animals")
+@animals_blueprint.route("/animals/", methods=["POST"])
+def add_animal():
+    name = request.form["name"]
+    type = request.form["type"]
+    dob = request.form["dob"]
+    age = request.form["age"]
+    notes = request.form["notes"]
+    owner = request.form["owner"]
+    owner_tel = request.form["owner_tel"]
+    owner_email = request.form["owner_email"]
+    vet_id = request.form["vet_id"]
+    vet = vet_repository.select(vet_id)
+    new_animal = Animal(name, type, dob, age, notes, owner, owner_email, owner_tel, vet)
+    animal_repository.save(new_animal)
+    return redirect("/animals")
 
 @animals_blueprint.route("/animals/<id>/delete", methods=['POST'])
 def delete_animal(id):
